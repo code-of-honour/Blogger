410c672909eff30dca70d6bc93d84ee80b985006
code-of-honour <brandon.james.chun@gmail.com>
2016-02-08 19:33:10 +1100

    Intialize repo

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router'
+import Routes from './routes'
+import ReduxPromise from 'redux-promise'
 
-import App from './components/app';
-import reducers from './reducers';
+import Reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  <Provider store={createStoreWithMiddleware(Reducers)}>
+    <Router history={browserHistory} routes={Routes}/>
   </Provider>
   , document.querySelector('.container'));
